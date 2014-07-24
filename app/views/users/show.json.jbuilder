d61ab5fc9bd4c7f10072c03385455d0f2eb49019
trejo08 <trejocola@gmail.com>
2014-07-24 00:10:18 -0600

    First commit

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :lastname, :username, :email, :password, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capres::Application.config.secret_key_base = '5c89eaa6bd98b8a9bcedda6c570b5b4197e88222eb86d4ac90d2d942754ac3c7d39f3be19713753e053aa888421b28743c88aad6bc5af450905cb499c0bc93e8'
